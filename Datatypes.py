3f9fa7fb8edab5655b16faebec2e63409256c375
Soham Ochani <soham.ochani4518@gmail.com>
2026-07-15 17:37:34 +0530

    Datatypes and Operators

---
+++ Datatypes.py
@@ -0,0 +1,48 @@
+#following are the data types in python
+#integer (int) 
+#strings(str)
+#float
+#boolean 
+#none
+
+a=5
+b="Soham"
+c=1.8
+age=34
+old=False
+d=None
+
+print (type(a))
+print (type(b))
+print (type(c))
+print (type(age))
+print (type(old))
+print (type(d))
+
+
+#int True T should be in uppercase
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Operators.py
@@ -0,0 +1,72 @@
+#Arithmetic operators 
+a=5
+b=2
+
+sum=a+b
+diff=a-b
+mult=a*b
+div=a/b
+remainder=a%b #remainder when a is divided by b
+power=a**b #a raised to the power of b
+
+print ("the sum is:",sum)
+print ("the difference is:",diff)
+print ("the product is:",mult)
+print ("the division is:",div)
+print("the remainder is :",remainder)
+print("a^b equalls",power)
+
+"'these are comments they can also be written ussing # and dont affect the code'"
+
+#relational/comparision operators
+# == , != , >= , <= ,< , >
+c=50
+d=20
+
+print(c == d) #False
+print(c != d) #True
+print(c<=d)#False
+print(c >=d)#True
+print(c < d)#False
+print(c > d)#True
+
+#Assignment Operators
+num=10
+num+=5
+print("number 1 is :",num)
+
+num2=20
+num2-=10
+print("umber 2 is :",num2)
+
+num3=5
+num3**=2
+print("number 3 is:",num3)
+
+num4=10
+num4/=5
+print("number 4 is :",num4)
+
+num5=10
+num5%=5
+print("number 5 is :",num5)
+
+num6=10
+num6*=50
+print("number 6 is :",num6)
+
+#logical operators
+# not and or
+e=50
+f=30
+print (not True)
+print(not (a == b))
+print (not(a != b))
+
+print((e != f) and (e > f))
+print((e == f) or (e > f))
+
+
+
+
+
