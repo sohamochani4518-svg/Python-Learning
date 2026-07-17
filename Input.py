f09297d27d0fef1946fa0c046b98f343c873a9fa
Soham Ochani <soham.ochani4518@gmail.com>
2026-07-17 17:11:42 +0530

    Completed python basics and one practise sheet

---
+++ Input.py
@@ -0,0 +1,15 @@
+name = input("enter your name")
+print("welcome",name)
+print("name of the applicant is :",name)
+
+
+val=input("enter your value")
+print(type(val))
+
+name=input("enter name")
+age=int(input("enter age"))
+marks=float(input("enter marks"))
+
+print("Welcome ",name)
+print("age=",age)
+print("marks=",marks)
---
+++ Python_Practise_Sheet1.py
@@ -0,0 +1,28 @@
+#WAP to input 2 numbers and print their sum
+
+#a=float(input("Enter number 1 : "))
+#b=float(input("Enter number 2 : "))
+
+#sum=a+b
+#print("The sum is",sum)
+
+
+#WAP to input side of a square and measure its area
+#s=float(input("Enter a value for side of the square : "))
+
+
+#area=s**2
+#print("The area of such sqaure with side",s,"equalls to : ",area)
+
+#WAP to input 2 floating type numbers and print their average 
+#n1=float(input("Enter number 1 : "))
+#n2=float(input("Enter number 2 : "))
+
+#print("The avg of ",n1,"and",n2,"is equall to : ",(n1+n2)/2)
+
+#WAP to input 2 integer numbers c and d . print True if c is greater than or equall to d or else print false
+
+c=float(input("Enter number 1 : "))
+d=float(input("Enter number 2 : "))
+
+print (c >= d)
