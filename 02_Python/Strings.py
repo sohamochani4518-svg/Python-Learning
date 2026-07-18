7443093caeabed4bc5837597bd88080b7b5ad3cc
Soham Ochani <soham.ochani4518@gmail.com>
2026-07-18 18:10:57 +0530

    Learnt about strings

---
+++ 02_Python/Strings.py
@@ -0,0 +1,18 @@
+str1='this is a string'
+str2="this is also a string but with double inveted commas"
+str3="'this is another string within triple inverted commas '"
+
+str4="' The tutor said , we can also use '' and "" commas while writing string names \n This can be done by using triple inverted commas to encase strings. '"
+
+print(str1)
+print(str2)
+print(str3)
+print(str4)
+print("length of str4 is  ",len(str4),"and that of str3 is ",len(str3))
+
+
+
+
+
+
+
